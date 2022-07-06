75ffbb522c0449d58ec96bf2714cf209023507c9
mahesh <maheshbhaise@gmail.comgit config --global user.email maheshbhaise@gmail.com>
2022-07-06 21:20:08 +0530

    first commit

---
+++ src/test1.java
@@ -0,0 +1,16 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		int age=17;
+		if(age>=18) {
+			System.out.println("applicable for voting");
+		}else{
+			System.out.println("not applicable for voting");
+		}
+	}
+
+}
